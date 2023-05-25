b59388e0b66793e6c7d263f1020305cb7068c274
Nice22 <benothouessou74@gmail.com>
2023-05-25 07:32:46 +0100

    update-post_put

--- Arene/API -1/post.php
+++ Arene/API -1/post.php
@@ -1,18 +1,14 @@
 <?php
-	$url = "http://localhost/API/Arene/API-1/cars.php";
-	$data = array('name' => 'PEC', 'description' => 'Pencil 2H', 'price' => '2.25', 'category' => '9');
+$url = "http://localhost/API/Arene/API -1/cars.php";
+$data = array('name' => 'PEC', 'description' => 'Pencil 2H', 'price' => '2.25', 'category' => '9');
 
-	// use key 'http' even if you send the request to https://...
-	$options = array(
-		'http' => array(
-			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-			'method'  => 'POST',
-			'content' => http_build_query($data)
-		)
-	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-	if ($result === FALSE) { /* Handle error */ }
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	var_dump($result);
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
 ?>
