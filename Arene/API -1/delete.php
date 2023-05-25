0e7fed5b10fcf7aed7641802cffa99374f1b83be
Nice22 <benothouessou74@gmail.com>
2023-05-25 10:07:20 +0100

    update-post_put

--- Arene/API -1/delete.php
+++ Arene/API -1/delete.php
@@ -1,10 +1,19 @@
 <?php
-$url = "http://localhost/API/Arene/API-1/cars.php"; // supprimer le produit 1
+$url = "http://localhost/API/Arene/API-1/cars.php?id=1"; // Supprimer le produit avec l'ID 1
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response  = curl_exec($ch);
+
+$response = curl_exec($ch);
+
 var_dump($response);
+
+if (!$response) {
+    return false;
+}
+
 curl_close($ch);
+
 ?>
