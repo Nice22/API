b02f558f1c8ff21c9248ed8797598bbd89eecb45
Nice22 <benothouessou74@gmail.com>
2023-05-26 12:31:23 +0100

    update_index

--- Arene/API -1/index.php
+++ Arene/API -1/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome_to_Cars_engine</title>
+    <title>Welcome to Cars engine</title>
 </head>
 <body>
-    Welcome
+    Bienvenue
 </body>
 </html>
